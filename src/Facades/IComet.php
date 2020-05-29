7d8d4055b7fd9576989cdbc09047576234114a05
huangdijia <huangdijia@gmail.com>
2020-05-29 10:05:29 +0800

    optimize comment

--- src/Facades/IComet.php
+++ src/Facades/IComet.php
@@ -6,15 +6,15 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see Huangdijia\IComet\Icomet
- * @method array sign(string $cname = '', int $expires = 60)
- * @method bool push($cname = '', $content = '')
- * @method bool batchPush(array $context =  [])
- * @method bool broadcast($content = '', array $cnames = null)
- * @method bool check(string $cname = '')
- * @method bool close(string $cname = '')
- * @method bool clear(string $cname = '')
- * @method array info(string $cname = '')
- * @method void psub(Closure $callback)
+ * @method static array sign(string $cname = '', int $expires = 60)
+ * @method static bool push($cname = '', $content = '')
+ * @method static bool batchPush(array $context =  [])
+ * @method static bool broadcast($content = '', array $cnames = null)
+ * @method static bool check(string $cname = '')
+ * @method static bool close(string $cname = '')
+ * @method static bool clear(string $cname = '')
+ * @method static array info(string $cname = '')
+ * @method static void psub(Closure $callback)
  */
 class IComet extends Facade
 {
